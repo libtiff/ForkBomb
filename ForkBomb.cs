f61e7c9ee69a81ebc9f18afc81fa8f181eb162ce
Antony Geo <6280539+libtiff@users.noreply.github.com>
2019-02-13 19:12:13 +0200

    Create ForkBomb.cs

---
+++ ForkBomb.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace ForkBomb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                System.Console.WriteLine(@"
+                                    ========================================
+                                      Fork Bomb - Operating Systems Course
+                                                AntonyGeorgiades
+                                    ========================================
+                                         |||| 
+                                         ||||
+                                         \__|
+                                          ||      
+                                          || 
+                                          ||
+                                          ||   
+                                            ");
+
+            bool Trigger = true;
+            //Creating an endless loop which will create childs (clones of this instance that will keep cloning)
+            while (Trigger == true)
+            {
+               // Process.Start(Assembly.GetExecutingAssembly().Location);
+            }
+        }
+    }
+}
